34d7b48196dd030fc7dcd24be4bfb3609a610490
Kevin Zweerink <kevinzweerink@gmail.com>
2017-06-20 09:36:29 -0400

    nav

--- js/onesoff.js
+++ js/onesoff.js
@@ -16,6 +16,39 @@ var ledger =
 	var prev = ledger.find(function (entry) { return entry.index == currentIndex - 1 });
 	var next = ledger.find(function (entry) { return entry.index == currentIndex + 1 });
 
-	console.log(prev);
-	console.log(next);
+	var nav = document.createElement('div');
+	nav.classList.add('nav');
+
+	var name = document.createElement('h1');
+	name.innerHTML = 'Ones Off';
+	nav.appendChild(name);
+
+	var actions = document.createElement('div');
+	actions.classList.add('actions');
+
+	if (prev) {
+		var prevLink = document.createElement('a');
+		prevLink.href = 'http://kevinzweerink.website/ones-off/i/' + prev.name + '/';
+		prevLink.innerHTML = '←';
+		actions.appendChild(prevLink);
+	}
+
+	if (next) {
+		var nextLink = document.createElement('a');
+		nextLink.href = 'http://kevinzweerink.website/ones-off/i/' + next.name + '/';
+		nextLink.innerHTML = '→'
+		actions.appendChild(nextLink);
+	}
+
+	nav.appendChild(actions);
+
+	document.documentElement.appendChild(nav);
+
+	document.addEventListener('keyup', function (e) {
+		if (e.keyCode == 37) {
+			prevLink.click();
+		} else if (e.keyCode == 39) {
+			nextLink.click();
+		}
+	});
 })();
